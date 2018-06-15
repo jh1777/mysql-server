794f04afb6a7f02b788d41a3b6641524fc1ea7a3
Administrator <joerg.hannemann@gmail.com>
2018-06-15 23:03:42 +0200

    No throws just 404 on error

--- api/controllers/gehaltController.js
+++ api/controllers/gehaltController.js
@@ -6,7 +6,7 @@ Array.prototype.contains = function(element){
 // https://www.sitepoint.com/using-node-mysql-javascript-client/
 exports.sql_test = function(req, res) {
   connection.query('SELECT * FROM Gehalt', (err,rows) => {
-	  if(err) throw err;
+	if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 	  //console.log(rows);
 	  res.json({ message: "SELECT statement working - retreived "+ rows.length +" rows"});
 	});
@@ -23,7 +23,7 @@ exports.gehaltMonatJahr = function(req, res) {
 		if (param != null) {
 			if (["AKP","Kantine","Brutto","Netto"].contains(param)) {
 			connection.query(`SELECT Jahr, sum(${param}) as Summe FROM Gehalt group by Jahr`, (err,rows) => {
-				if(err) throw err;
+				if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 
 				//console.log(rows);
 				if (rows.length > 0) {
@@ -34,7 +34,7 @@ exports.gehaltMonatJahr = function(req, res) {
 		}
 		if (monat == null && jahr != null) {
 			connection.query(`SELECT Jahr, sum(Brutto) as Brutto_Summe, sum(Netto) as Netto_Summe, round(avg(Brutto),2) as Brutto_Avg, round(avg(Netto),2) as Netto_Avg, round(sum(Kantine), 2) as Kantine, round(sum(AKP),2) as AKP FROM Gehalt where Jahr = ${jahr}`, (err,rows) => {
-				if(err) { res.status(404).send('Sorry, we cannot find Jahr: '+jahr); }//throw err;
+				if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 
 				//console.log(rows);
 				if (rows.length > 0) {
@@ -44,7 +44,7 @@ exports.gehaltMonatJahr = function(req, res) {
 		} 
 		if (jahr == null && monat != null) {
 			connection.query(`SELECT Monat, round(avg(Brutto),2) as Brutto_Avg, round(avg(Netto),2) as Netto_Avg FROM Gehalt where Monat = ${monat}`, (err,rows) => {
-				if(err) throw err;
+				if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 
 				if (rows.length > 0) {
 					res.json(rows[0]); 
@@ -53,7 +53,7 @@ exports.gehaltMonatJahr = function(req, res) {
 		}
 		if (monat != null && jahr != null) {
 			connection.query(`SELECT * FROM Gehalt where Monat = ${monat} and Jahr = ${jahr}`, (err,rows) => {
-				if(err) throw err;
+				if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 
 				if (rows.length > 0) {
 					res.json(rows[0]); 
@@ -73,7 +73,7 @@ exports.insertGehalt = function (req, res) {
 	const gehaltRow = { Monat: monat, Jahr: jahr, Brutto: brutto, Netto: netto, AKP: akp, Kantine: kantine };
 	console.log(gehaltRow);
 	connection.query('INSERT INTO Gehalt SET ?', gehaltRow, (err, result) => {	
-		if(err) throw err;
+		if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 	  console.log('Last insert ID:', result.insertId);
 		res.json(gehaltRow);
 	});
@@ -85,7 +85,7 @@ exports.deleteGehalt = function (req, res) {
 	const gehaltRow = { Monat: monat, Jahr: jahr };
 
 	connection.query('DELETE FROM Gehalt Where Monat = ? and Jahr = ?', [monat, jahr], (err, result) => {	
-		if(err) throw err;
+		if(err) { res.status(404).send('Sorry, we cannot find that'); }//throw err;
 		res.json(gehaltRow);
 	});
 };
